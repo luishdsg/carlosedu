c6a322fe8f332774e0fb7cd1cacb587d9d80fc51
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-01-30 22:05:00 -0300

    create

---
+++ assets/js/main.js
@@ -0,0 +1,76 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".BitNickltda",
+  `Criei contartos com para blockchain da ETH com integração na exchange da mercado bitcoin,
+  também fui responsável pela criação e divulgação de produtos e Designer de marking.`,
+  "Programador Back-end",
+  "BitNick.ltda",
+  "Jun 2020 - Nov 2020"
+);
+
+hoverChangeExperience(
+  ".JBS",
+  `Fui Operador de produção, contribui para o crecsimento da empresa auxiliando no manuseio das paleteiras eletrônicas e estocagem, 
+  e fui membro da equipe de logística de carga.`,
+  "Conferênte de carga",
+  "JBS",
+  "Maio 2022 - Jan 2023"
+);
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
+);
+hoverChangeDescription(
+  ".sass",
+  "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
+);
+hoverChangeDescription(
+  ".react",
+  "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
+);
+hoverChangeDescription(
+  ".next",
+  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
+);
+hoverChangeDescription(
+  ".styled",
+  "styled-components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "Tailwind CSS é um framework CSS que nos fornece classes utilitárias com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".typescript",
+  "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
+);
+hoverChangeDescription(
+  ".radix",
+  "Radix é uma biblioteca que fornece componentes acessíveis e sem estilos para a criação de aplicação em React."
+);
+hoverChangeDescription(
+  ".cypress",
+  "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
+);
+hoverChangeDescription(
+  ".storybook",
+  "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+);
